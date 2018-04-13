eb51d9b1f991591575e9abc6b97eba8028a08760
Petr Kočí <petr.koci@samizdat.cz>
2018-04-13 16:38:58 +0200

    anketa hotová

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// JS soubory ve složce /styles/ se do výsledného článku zakompilují automaticky
+$(document).ready(function() {
+  $.getJSON( "../data/data.json", function(data) {
+    console.log(data);
+    $(data).each(function(i) {
+      if (this.o1.length>0) {$("<div class='respondent'></div>").appendTo("#anketa")};
+      if (this.o1.length==0) {$("<div class='respondent neodpovedel'></div>").appendTo("#anketa")};
+      $(".respondent").last().append("<img class='portret' src='" + this.f + "'>");
+      $(".respondent").last().append("<p class='jmeno'><strong>" + this.j + " " + this.p + "</strong></p>");
+      if (this.s=="ANO") {$(".jmeno").last().css("background-color", "green")};
+      if (this.s=="NE") {$(".jmeno").last().css("background-color", "red")};
+      if (this.s=="NEUTRÁLNÍ") {$(".jmeno").last().css("background-color", "grey")};
+      if (this.o1.length>0) {$(".respondent").last().append("<p class='veta'>&bdquo;" + this.o1 + "&ldquo;</p>")};
+      if (this.o1.length==0) {$(".respondent").last().append("<p class='veta' style='color:red;'>Bez odpovědi</p>")};
+    });
+
+  });
+});
